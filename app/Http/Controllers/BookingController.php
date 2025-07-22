50de9ae6870e44047b7d852241b1a60111af075e
igliyeva29 <igliyevameryem2@gmail.com>
2025-07-22 21:49:53 +0500

    2.1.0

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Booking;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
-    //
+    public function index()
+    {
+        $bookings = Booking::get();
+
+        return view('bookings.index')->with(
+            [
+                'bookings' => $bookings,
+            ]
+        );
+    }
 }
--- app/Http/Controllers/PassengerController.php
+++ app/Http/Controllers/PassengerController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Passenger;
 use Illuminate\Http\Request;
 
 class PassengerController extends Controller
 {
-    //
+    public function index()
+    {
+        $passengers = Passenger::get();
+
+        return view('passengers.index')->with(
+            [
+                'passengers' => $passengers,
+            ]
+        );
+    }
 }
