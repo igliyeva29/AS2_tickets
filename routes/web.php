6b3ba9b225826903347130c1877cec01c0eb02e1
igliyeva29 <igliyevameryem2@gmail.com>
2025-07-22 21:47:48 +0500

    2.0.0

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\FareController;
+use App\Http\Controllers\PassengerController;
 use App\Http\Controllers\SeatController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
@@ -10,3 +12,5 @@ Route::get('', [HomeController::class, 'index'])->name('home');
 Route::get('flights', [FlightController::class, 'index'])->name('flights');
 Route::get('fares', [FareController::class, 'index'])->name('fares');
 Route::get('seats', [SeatController::class, 'index'])->name('seats');
+Route::get('bookings', [BookingController::class, 'index'])->name('bookings');
+Route::get('passengers', [PassengerController::class, 'index'])->name('passengers');
